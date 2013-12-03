f1d156321cb30fdfa24a59f4be8a0f9b8d76837e
stefanwas <swtemp001@gmail.com>
2013-12-03 17:03:35 +0100

    initial commit

---
+++ app/js/dashboard.js
@@ -0,0 +1,11 @@
+
+// Application main module definition
+
+var dashboard = angular.module('dashboard', [
+//     'ngResource',
+     'dashboard.directives',
+     'dashboard.controllers',
+     'dashboard.services']);
+
+
+
