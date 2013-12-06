53c44d946069ecfb834dea973b9b392eaf62ddae
stefanwas <swtemp001@gmail.com>
2013-12-06 11:54:53 +0100

    added Filter service

--- app/js/dashboard-services.js
+++ app/js/dashboard-services.js
@@ -1,6 +1,40 @@
 var dashboardServices = angular.module('dashboard.services', [ 'ngResource' ]);
 
-dashboardServices.service('filterContentService', function() {
+dashboardServices.service('filterContentService', ['$resource', function($resource) {
+    var filterDataProvider = 'http\\://localhost\\:8000/provider/rest/json/filterData';
+
+    this.getFlatFilterContent = function(filterName) {
+        ///////////////////////////////////
+        var filterDataService = $resource(filterDataProvider);
+        var flatFilterMapResult = undefined;
+
+        filterDataService.get(function(result) {
+
+//    		$scope.flatFilters = result.filterData.flatFilters;
+//    		$scope.hierarchicalFilters = result.filterData.hierarchicalFilters;
+            var flatFilters = result.filterData.flatFilters;
+//    		var hierarchicalFilters = result.filterData.hierarchicalFilters;
+
+            var flatFiltersMap = {};
+            for (var i=0; i < flatFilters.length; i++) {
+                var filter = flatFilters[i];
+                flatFiltersMap[filter.name] = filter.items;
+            }
+            flatFilterMapResult = flatFiltersMap;
+
+
+            console.log('This was the result 1:', result.filterData.flatFilters);
+            console.log('This was the result 2:', result.filterData.hierarchicalFilters);
+
+        });
+
+        ///////////////////////////////
+        return "";
+    };
+}
+]);
+
+dashboardServices.service('filterContentServiceX', function() {
 	
 	var filterDataProvider = 'http\\://localhost\\:8000/provider/rest/json/filterData';
 	
@@ -35,13 +69,13 @@ dashboardServices.service('filterContentService', function() {
 
 
 dashboardServices.factory('Filter', [ '$resource', function($resource) {
-	return $resource('filter/:filterName.json', {}, {
-		query : {
+	return $resource('filter/:filterName', {}, {
+		getFilterData : {
 			method : 'GET',
 			params : {
-				filterName : 'filters'
+				filterName : '@filterName'
 			},
-			isArray : true
+			isArray : false
 		}
 	});
 } ]);
