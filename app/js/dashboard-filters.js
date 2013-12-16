318dd8b11ece91dd3e3bca42643a37bfb1ddc342
stefanwas <swtemp001@gmail.com>
2013-12-16 11:56:36 +0100

    added filter highlighting

--- app/js/dashboard-filters.js
+++ app/js/dashboard-filters.js
@@ -1,9 +1,17 @@
 var dashboardFilters = angular.module('dashboard.filters', ['ngSanitize']);
 
-dashboardFilters.filter('simpleFilter', function () {
-    return function (input, query) {
-    	return input.replace(RegExp('('+ query + ')', 'g'), '<span class="super-class">$1</span>');
-//        return input + '!!';
+dashboardFilters.filter('highlightFilter', function () {
+//    return function (input, query) {
+//        return input.replace(RegExp('('+ query + ')', 'g'), '<span class="super-class">$1</span>');
+//    }
+
+    function escapeRegexp(queryToEscape) {
+        return queryToEscape.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
     }
+
+    return function(matchItem, query) {
+        return query ? matchItem.replace(new RegExp(escapeRegexp(query), 'gi'), '<span class="super-class">$&</span>') : matchItem;
+    };
+
 });
 
